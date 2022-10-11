7c5287cfd99a0858beff907bfcd5f6b600e6da45
Kazuki Taninaka <onelive555@gmail.com>
2022-10-11 17:05:05 +0200

    wip

---
+++ src/cars.ts
@@ -0,0 +1,44 @@
+export const cars = [
+  {
+    id: 1,
+    name: "Ferrari 250 GTO",
+    image:
+      "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
+    price: 70,
+  },
+  {
+    id: 2,
+    name: "1959 Cadillac Eldorado",
+    image:
+      "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
+    price: 70,
+  },
+  {
+    id: 3,
+    name: "Porsche 911",
+    image:
+      "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
+    price: 70,
+  },
+  {
+    id: 4,
+    name: "Shelby Mustang Cobra",
+    image:
+      "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
+    price: 70,
+  },
+  {
+    id: 5,
+    name: "1978 Pontiac Firebird Trans Am",
+    image:
+      "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
+    price: 70,
+  },
+  {
+    id: 6,
+    name: "Delorean DMC 12",
+    image:
+      "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
+    price: 70,
+  },
+];
--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -1,51 +1,8 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import Card from "./Card";
+import { cars } from "../cars";
 
 const Cars = () => {
-  const cars = [
-    {
-      id: 1,
-      name: "Ferrari 250 GTO",
-      image:
-        "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
-      price: 70,
-    },
-    {
-      id: 2,
-      name: "1959 Cadillac Eldorado",
-      image:
-        "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
-      price: 70,
-    },
-    {
-      id: 3,
-      name: "Porsche 911",
-      image:
-        "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
-      price: 70,
-    },
-    {
-      id: 4,
-      name: "Shelby Mustang Cobra",
-      image:
-        "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
-      price: 70,
-    },
-    {
-      id: 5,
-      name: "1978 Pontiac Firebird Trans Am",
-      image:
-        "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
-      price: 70,
-    },
-    {
-      id: 6,
-      name: "Delorean DMC 12",
-      image:
-        "https://cdn2.lamag.com/wp-content/uploads/sites/6/2021/09/car001_1500x1000px.jpg",
-      price: 70,
-    },
-  ];
   return (
     <SimpleGrid minChildWidth={"330px"}>
       {cars.map((car, i) => {
